b79e93ca72751da3e095b37860206aed9f0ba242
Mark McGranaghan <mmcgrana@gmail.com>
2013-01-04 11:40:58 -0800

    spaces

--- web.rb
+++ web.rb
@@ -5,16 +5,16 @@ DB = Sequel.connect(ENV["DATABASE_URL"], max_connections: 5)
 
 class Web < Sinatra::Base
   get "/no-op" do
-  	"no-op\n"
+    "no-op\n"
   end
 
   get "/sleep-ruby" do
-  	sleep(0.100)
-  	"ruby-sleep\n"
+    sleep(0.200)
+    "ruby-sleep\n"
   end
 
   get "/sleep-pg" do
-  	DB.run("select pg_sleep(0.100)")
-  	"pg-sleep\n"
+    DB.run("select pg_sleep(0.200)")
+    "pg-sleep\n"
   end
 end
